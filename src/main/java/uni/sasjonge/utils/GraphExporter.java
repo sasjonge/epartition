08b583904f80f97cfd1d4a1445d169e0ec5ba778
sasjonge <sasjonge@uni-bremen.de>
2019-06-28 16:07:58 +0200

    Commented the graphexporter

--- src/main/java/uni/sasjonge/utils/GraphExporter.java
+++ src/main/java/uni/sasjonge/utils/GraphExporter.java
@@ -32,10 +32,13 @@ import uni.sasjonge.Settings;
  */
 public class GraphExporter {
 
+	// Hierachy of the given ontology
 	static OntologyHierarchy ontHierachy;
+	// Manages the labels for the given ontology
 	static OntologyDescriptor ontDescriptor;
 
-	static Map<String, Integer> vertexToAxiomsCount = null;
+	// Counts how many axioms belon to a cc
+	static Map<String, Integer> ccToAxiomCount = null;
 
 	/**
 	 * Exports g in graphML to outputPath Every node and edge is shown as is in the
@@ -66,6 +69,7 @@ public class GraphExporter {
 		// Initizalize Filewriter and export the corresponding graph
 		FileWriter fw;
 		try {
+			// Simpy output the graph created by the algorithm
 			fw = new FileWriter(outputPath);
 			exporter.exportGraph(g, fw);
 			fw.flush();
@@ -89,14 +93,14 @@ public class GraphExporter {
 	public static String exportCCStructureGraph(Graph<String, DefaultEdge> g, OWLOntology ontology,
 			Map<String, Set<OWLAxiom>> vertexToAxiom, String outputPath) throws ExportException {
 		
+		// Create a string builder for the output
 		StringBuilder builder = new StringBuilder();
 
 		// Classes and individuals of the given ontology
 		Set<String> classes = getClassesForOntology(ontology);
-
 		Set<String> individuals = getIndividualsForOntology(ontology);
 
-		// Find the connected components
+		// Used to find the connected components
 		ConnectivityInspector<String, DefaultEdge> ci = new ConnectivityInspector<>(g);
 
 		// Create the graph we want to output
@@ -110,39 +114,47 @@ public class GraphExporter {
 
 		// Map the vertexes to the corresponding set of axioms, classes, individuals
 		Map<String, Set<String>> vertexToClasses = new HashMap<>();
-
 		Map<String, Set<String>> vertexToIndividuals = new HashMap<>();
-
 		Map<String, Set<OWLAxiom>> vertexToAxioms = null;
+		
+		// If we want to show axioms in the output graph
 		if (Settings.SHOW_AXIOMS) {
-			vertexToAxioms = getAxiomsToVertex(ci.connectedSets(), vertexToAxiom);
-			GraphExporter.vertexToAxiomsCount = new HashMap<>();
+			
+			// Get the axioms for the cc
+			vertexToAxioms = getCCToAxioms(ci.connectedSets(), vertexToAxiom);
+			
+			// Create the vertexToAxiomsCount Hashmap
+			GraphExporter.ccToAxiomCount = new HashMap<>();
+			
+			// To save the number number of axioms for this cc
 			for (Entry<String, Set<OWLAxiom>> e : vertexToAxioms.entrySet()) {
-				vertexToAxiomsCount.put(e.getKey(), e.getValue().size());
+				ccToAxiomCount.put(e.getKey(), e.getValue().size());
 			}
 		} else {
-			vertexToAxiomsCount = getAxiomsToVertexCount(ci.connectedSets(), vertexToAxiom);
+			// Else just save the axiom to vertex count
+			ccToAxiomCount = getCCToAxiomsCount(ci.connectedSets(), vertexToAxiom);
 		}
 		
-		// Save how many axioms there are per vertex
 
-		// Map vertexes to classes
-		// Map vertexes to individuals
+		// For each connected component
 		for (Set<String> cc : ci.connectedSets()) {
 
-			// classes and individuals
-			// start with same baseset (alle elements of the cc)
+			// Create the set of all classes and all individuals for this cc
+			// classes and individuals start with same baseset (alle elements of the cc)
 			Set<String> classesForThisCC = cc.stream().map(e -> OntologyDescriptor.getCleanName(e))
 					.collect(Collectors.toSet());
 			Set<String> individualsForThisCC = new HashSet<>(classesForThisCC);
 
+			// Take the intersection of classesForThisCC (which simply contains all vertexes in the cc)
+			// and all classes in the ontology to get the classes in this cc
 			classesForThisCC.retainAll(classes);
 
+			// If there are classes in this cc
 			if (classesForThisCC.size() > 0) {
 				vertexToClasses.put(cc.toString() + "", classesForThisCC);
 			}
 
-			// individuals
+			// Repeat the same for the individuals
 			individualsForThisCC.retainAll(individuals);
 
 			if (individualsForThisCC.size() > 0) {
@@ -150,37 +162,54 @@ public class GraphExporter {
 			}
 		}
 
+		// Add the opening bracket to the builder
 		builder.append("[");
+		
 		// Add vertexes for all connected components
 		for (Set<String> cc : ci.connectedSets()) {
-			// System.out
-			// .println("Number of axioms for " + cc.toString() + " is " +
-			// vertexToAxioms.get(cc.toString() + ""));
-			if (vertexToAxiomsCount.get(cc.toString() + "") != null) {
-				builder.append(vertexToAxiomsCount.get(cc.toString() + "").toString() + ", ");
+
+			// If there is at least one axiom in this cc
+			if (ccToAxiomCount.get(cc.toString() + "") != null) {
+				
+				// Add the number of axioms for this cc to the string
+				builder.append(ccToAxiomCount.get(cc.toString() + "").toString() + ", ");
+				
+				// Save out name for the cc (used as a key)
 				ccToVertexName.put(cc, cc.toString() + "");
+				
+				// Add the cc as a vertex
 				ccGraph.addVertex(cc.toString() + "");
+				
+				// Save the cc as a cc with axioms
 				ccWithAxioms.add(cc);
 				i++;
 			}
-		}
-		;
+		};
+		
+		// Close the string till her
 		builder.append("], ");
 		
 		// Create a map of map as a matrix for connectedness
 		Map<String, Map<String, Integer>> matrix = new HashMap<>();
+		//For each cc
 		for (Set<String> cc : ci.connectedSets()) {
+			// Add the cc to the map
 			matrix.put(cc.toString() + "", new HashMap<>());
+			// For each cc2
 			for (Set<String> cc2 : ci.connectedSets()) {
+				// Use cc as a key in the matrix for cc2 and 0
 				matrix.get(cc.toString() + "").put(cc2.toString() + "", new Integer(0));
 			}
 		}
 
-		// Create a map from subconcepts to the cc that contains it
+		// Create a map from property vertexes to the cc that contains it
 		Map<String, Set<String>> role0ToCC = new HashMap<>();
 		Map<String, Set<String>> role1ToCC = new HashMap<>();
+		// For each cc with axioms
 		ccWithAxioms.stream().forEach(cc -> {
+			// For each vertex
 			cc.stream().forEach(subcon -> {
+				// If the vertex has the form of a property vertex save it
 				if (subcon.endsWith(Settings.PROPERTY_0_DESIGNATOR)) {
 					role0ToCC.put(subcon, cc);
 				} else if (subcon.endsWith(Settings.PROPERTY_1_DESIGNATOR)) {
@@ -192,12 +221,14 @@ public class GraphExporter {
 		//find corresponding property nodes (same prefix, ending
 				// with 0 or 1)
 				// remember the name of properties for the edges
-		
 		Map<DefaultEdge, Set<String>> nameForEdge = new HashMap<>();
 		Map<String, Set<String>> vertexToProperties = new HashMap<>();
 		
+		// For each role0 we saved (this CAN contain data properties)
 		for (Entry<String, Set<String>> roleToCC : role0ToCC.entrySet()) {
+			// Get the name of the role0
 			String role0 = roleToCC.getKey();
+			// And create the cc for role1
 			Set<String> ccOfRole1 = role1ToCC.get(role0.substring(0, role0.length() - Settings.PROPERTY_0_DESIGNATOR.length()) + Settings.PROPERTY_1_DESIGNATOR);
 			
 			// If there is a "partner-role"
@@ -207,6 +238,7 @@ public class GraphExporter {
 				// edge between the corresponding cc's
 				
 				DefaultEdge edge = null;
+				// Get all edges that are between the cc of role0 and role 1
 				Set<DefaultEdge> edgeList = ccGraph.getAllEdges(ccToVertexName.get(roleToCC.getValue()),
 						ccToVertexName.get(ccOfRole1));
 				if (edgeList.isEmpty()) {
@@ -215,6 +247,7 @@ public class GraphExporter {
 					matrix.get(roleToCC.getValue() + "").put(ccOfRole1.toString() + "",new Integer(1));
 				} else if (edgeList.size() == 1) {
 					// If the edgelist is not empty it should only contain one element.
+					// So do nothing here (beside increasing the iterator)
 					edge = edgeList.iterator().next();
 				}
 
@@ -224,7 +257,6 @@ public class GraphExporter {
 				}
 
 				// Then add the name of the edge
-				// System.out.println(getCleanName(subcon.substring(0, subcon.length() - 1)));
 				nameForEdge.get(edge)
 						.add(OntologyDescriptor.getCleanName(role0.substring(0, role0.length() - Settings.PROPERTY_1_DESIGNATOR.length())));
 
@@ -233,6 +265,7 @@ public class GraphExporter {
 				// if there is no "partner" role role0 is a dataproperty
 				// if role0 and role1 have the same cc, they are in the same partition
 				String ccName = ccToVertexName.get(roleToCC.getValue());
+				// Add the property to the vertex of the xx
 				if (vertexToProperties.get(ccName) == null) {
 					vertexToProperties.put(ccName, new HashSet<>());
 				}
@@ -242,7 +275,7 @@ public class GraphExporter {
 			}
 		}
 		
-		// Save matrix
+		// Save matrix to the string
 		builder.append("[");
 		
 		for (Entry<String, Map<String, Integer>> row : matrix.entrySet()) {
@@ -263,7 +296,7 @@ public class GraphExporter {
 		exporter.setVertexLabelProvider(new ComponentNameProvider<String>() {
 			@Override
 			public String getName(String vertex) {
-				return ontDescriptor.getLabelForConnectedComponent(vertexToAxiomsCount.get(vertex),
+				return ontDescriptor.getLabelForConnectedComponent(ccToAxiomCount.get(vertex),
 						vertexToClasses.get(vertex), vertexToProperties.get(vertex), vertexToIndividuals.get(vertex))
 						+ (Settings.SHOW_AXIOMS ? "\n" + ontDescriptor.getAxiomString(vertexToAxioms.get(vertex)) : "");
 				// + ((axioms.size() < 16) ? "_________\n CC: \n" + vertex : "");
@@ -280,7 +313,6 @@ public class GraphExporter {
 			}
 		});
 
-		// exporter.setVertexLabelAttributeName("custom_vertex_label");
 		// Initizalize Filewriter and export the corresponding graph
 		FileWriter fw;
 		try {
@@ -295,46 +327,95 @@ public class GraphExporter {
 		return builder.toString();
 	}
 
-	private static Map<String, Set<OWLAxiom>> getAxiomsToVertex(List<Set<String>> connectedSets,
+	/**
+	 * Given a List of connected components and a map mapping vertexes to sets of axioms
+	 * return a map, that maps the cc to their axioms
+	 * 
+	 * @param connectedSets
+	 * @param vertexToAxiom
+	 * @return Mapping from cc to axioms
+	 */
+	private static Map<String, Set<OWLAxiom>> getCCToAxioms(List<Set<String>> connectedSets,
 			Map<String, Set<OWLAxiom>> vertexToAxiom) {
 
-		Map<String, Set<OWLAxiom>> vertexToAxioms = new HashMap<>();
+		// Create the map to map cc to axioms
+		Map<String, Set<OWLAxiom>> ccToAxioms = new HashMap<>();
+		
+		// For each cc
 		for (Set<String> cc : connectedSets) {
+			
+			// For each vertex in this cc
 			for (String vert : cc) {
+				
+				// If there are are axioms for this vertex
 				if (vertexToAxiom.get(vert) != null) {
-					if (!vertexToAxioms.containsKey(cc.toString() + "")) {
-						vertexToAxioms.put(cc.toString() + "", new HashSet<>());
+					// If the cc isn't already a key in the map create the entry
+					if (!ccToAxioms.containsKey(cc.toString() + "")) {
+						ccToAxioms.put(cc.toString() + "", new HashSet<>());
 					}
-					vertexToAxioms.get(cc.toString() + "").addAll(vertexToAxiom.get(vert));
+					// and add the axioms
+					ccToAxioms.get(cc.toString() + "").addAll(vertexToAxiom.get(vert));
 
 				}
 			}
 		}
 
-		return vertexToAxioms;
+		return ccToAxioms;
 	}
 
-	private static Map<String, Integer> getAxiomsToVertexCount(List<Set<String>> connectedSets,
+	/**
+	 * Giiven a list of connected components and a map mapping vertexes to axioms
+	 * return a map mapping the cc to the number of vertexes
+	 * 
+	 * @param connectedSets
+	 * @param vertexToAxiom
+	 * @return Mapping of cc's to their number of axioms
+	 */
+	private static Map<String, Integer> getCCToAxiomsCount(List<Set<String>> connectedSets,
 			Map<String, Set<OWLAxiom>> vertexToAxiom) {
-		Map<String, Integer> vertexToAxioms = new HashMap<>();
+		
+		// Create the map to retunr
+		Map<String, Integer> ccToAxiomsCount = new HashMap<>();
+		
+		// For each connected component
 		for (Set<String> cc : connectedSets) {
+			
+			// Counter for the number of axioms for this cc
 			int count = 0;
+			
+			// For each vertex of the connected component
 			for (String vert : cc) {
 
-				count = vertexToAxiom.get(vert) != null ? count + vertexToAxiom.get(vert).size() : count;
+				// If there are axioms for this vertex add their number to the counter
+				count = vertexToAxiom.get(vert) != null ?					
+						count + vertexToAxiom.get(vert).size() : count;
 
 			}
-			vertexToAxioms.put(cc.toString() + "",count);
+			
+			// Map the cc to the number of axioms
+			ccToAxiomsCount.put(cc.toString() + "",count);
 		}
 
-		return vertexToAxioms;
+		return ccToAxiomsCount;
 	}
 
+	/**
+	 * Returns the named classes for the ontology
+	 * 
+	 * @param ontology
+	 * @return Set of name of classes
+	 */
 	private static Set<String> getClassesForOntology(OWLOntology ontology) {
 		return ontology.classesInSignature().map(e -> OntologyDescriptor.getCleanNameOWLObj(e))
 				.collect(Collectors.toSet());
 	}
 
+	/**
+	 * Returns the named individuals
+	 * 
+	 * @param ontology
+	 * @return Set of name of individuals
+	 */
 	private static Set<String> getIndividualsForOntology(OWLOntology ontology) {
 		return ontology.individualsInSignature().map(e -> OntologyDescriptor.getCleanNameOWLObj(e))
 				.collect(Collectors.toSet());
@@ -351,10 +432,10 @@ public class GraphExporter {
 			// Initialize a hierachy structure for the ontology
 			ontHierachy = new OntologyHierarchy(ontology);
 
+			// Initialized a object that manages the labels for the ontology
 			ontDescriptor = new OntologyDescriptor(ontHierachy, ontology);
-			// System.out.println(depthToClasses);
+			
 		} catch (OWLOntologyCreationException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
